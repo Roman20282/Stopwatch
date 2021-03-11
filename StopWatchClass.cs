6ead07049a351fcb93a49fe730095add0f2786be
Roman20282 <79865806+Roman20282@users.noreply.github.com>
2021-03-11 13:29:40 +0200

    Add files via upload

---
+++ StopWatchClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    internal class StopWatchClass
+    {
+        public DateTime Start()
+        {
+            DateTime startMoment = DateTime.Now;
+            return startMoment;
+        }
+
+        public DateTime Stop()
+        {
+            DateTime stopMoment = DateTime.Now;
+            return stopMoment;
+        }
+    }
+}
